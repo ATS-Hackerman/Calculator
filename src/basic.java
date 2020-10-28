d21f420d59ff1708f49cee9dfc6720ce8d43ce14
Adrian Tokle Storset <55131770+ATS-Hackerman@users.noreply.github.com>
2020-10-28 23:51:34 +0100

    Added first methods

--- src/basic.java
+++ src/basic.java
@@ -1,2 +1,13 @@
 public class basic {
+    public static double Addition(double a, double b){
+        double sum = 0.0;
+
+        return sum;
+    }
+
+    public static double Subtraction(double a, double b){
+        double sum = 0.0;
+
+        return sum;
+    }
 }
