804ca55f8904d6a28d17afa01b4124179a3b2122
Adrian Tokle Storset <55131770+ATS-Hackerman@users.noreply.github.com>
2020-10-31 20:02:57 +0100

    Added divition method

--- src/basic.java
+++ src/basic.java
@@ -1,13 +1,25 @@
 public class Basic {
     public static double Addition(double a, double b){
-        double sum = 0.0;
+        double sum;
         sum = a + b;
         return sum;
     }
 
     public static double Subtraction(double a, double b){
-        double sum = 0.0;
+        double sum;
         sum = a - b;
         return sum;
+    }
+
+    public static double Multiplication(double a, double b){
+        double product;
+        product = a * b;
+        return product;
+    }
+
+    public static double Divition(double a, double b){
+        double result;
+        result = a / b;
+        return result;
     }
 }
--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("20 + 30 = " + Basic.Addition(20,30));
         System.out.println("20 - 30 = " + Basic.Subtraction(20,30));
+        System.out.println("20 * 30 = " + Basic.Multiplication(20,30));
     }
 }
