9cdac1aba98090436eb7324a5a929973d8ea4499
Adrian Tokle Storset <55131770+ATS-Hackerman@users.noreply.github.com>
2020-11-13 23:13:14 +0100

    Moved factorial method to new class ' Intermidiate'

---
+++ src/Intermidiate.java
@@ -0,0 +1,9 @@
+public class Intermidiate {
+    public static int Factorial(int a){
+        int result = 1;
+        for(int i = 1; i <= a; i++){
+            result = result * i;
+        }
+        return result;
+    }
+}
