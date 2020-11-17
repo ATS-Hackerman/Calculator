038d7a402182d3b8efef7eb591a06cec6ad0d09d
Adrian Tokle Storset <55131770+ATS-Hackerman@users.noreply.github.com>
2020-11-17 17:14:03 +0100

    Added volume methods to Geometry.java

--- src/Geometry.java
+++ src/Geometry.java
@@ -4,8 +4,8 @@ public class Geometry {
         return ((length * height) / 2);
     }
 
-    public static double AreaSquare(int side){
-        return (side * side);
+    public static double AreaSquare(int edge){
+        return (edge * edge);
     }
 
     public static double AreaRectangle(int length, int width){
@@ -14,5 +14,17 @@ public class Geometry {
 
     public static double AreaCircle(int radius){
         return (Math.PI * radius * radius);
+    }
+
+    public static double VolumeTetrahedron(int edge){
+        return (Math.pow(edge, 3) / (6 * Math.sqrt(2)));
+    }
+
+    public static double VolumeCube(int edge){
+        return Math.pow(edge, 3);
+    }
+
+    public static double VolumeSphere(int radius){
+        return ((4 * Math.PI * Math.pow(radius, 3)) / 3);
     }
 }
