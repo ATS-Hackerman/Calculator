d7edbb5dea6b427701092e17e54bb4a7c2586df0
Adrian Tokle Storset <55131770+ATS-Hackerman@users.noreply.github.com>
2021-05-15 20:42:29 +0200

    :sparkles: Add swing objects

--- src/app/Calculator.java
+++ src/app/Calculator.java
@@ -5,12 +5,21 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Calculator implements ActionListener {
+    
+    JFrame frame;
+    JTextField tectfield;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[8];
+    JButton addButton, subtractButton, multiplyButton, divideButton;
+    JButton decimalButton, equalsButton, deleteButton, clearButton;
+    JPanel panel;
+    
     Calculator() {
-
+        
     }
 
     public static void main(String[] args) {
-        
+        Calculator calculator = new Calculator();
     }
     
     @Override
