5fc8e45800150f57077fa3ca905314f1c6b73a6a
Adrian Tokle Storset <55131770+ATS-Hackerman@users.noreply.github.com>
2021-05-15 20:35:32 +0200

    :heavy_plus_sign: Implement ActionListener

--- src/app/Calculator.java
+++ src/app/Calculator.java
@@ -1,5 +1,20 @@
 package app;
 
-public class Calculator {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator implements ActionListener {
+    Calculator() {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
     
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
 }
