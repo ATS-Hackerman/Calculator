18dfa9c4d46e3972fe260df1ed55628428b366e4
Adrian Tokle Storset <55131770+ATS-Hackerman@users.noreply.github.com>
2021-05-16 13:36:24 +0200

    :bulb: Add simple comments

--- src/app/Calculator.java
+++ src/app/Calculator.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Calculator implements ActionListener {
-    
+    //Swing objects setup
     JFrame frame;
     JTextField textfield;
     JButton[] numberButtons = new JButton[10];
@@ -11,18 +11,20 @@ public class Calculator implements ActionListener {
     JButton addButton, subtractButton, multiplyButton, divideButton;
     JButton decimalButton, equalsButton, deleteButton, clearButton;
     JPanel panel;
-
+    //Font Custimisation
     Font calcFont = new Font("Courier New", Font.BOLD, 30);
 
     double num1 = 0, num2 = 0, result = 0;
     double operator;
     
+    //Constructor
     Calculator() {
+        //Frame 
         frame = new JFrame("Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420, 550);
         frame.setLayout(null);
-        
+        //Screen for calculator output
         textfield = new JTextField();
         textfield.setBounds(50, 25, 300, 50);
         textfield.setFont(calcFont);
@@ -32,6 +34,7 @@ public class Calculator implements ActionListener {
         frame.setVisible(true);
     }
 
+    //Running method
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
     }
